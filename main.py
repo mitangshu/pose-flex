c185e729168fbdefc41e742d8c394e1ee5fc1eff
Mitangshu Chatterjee <mitangshu@indifoot.com>
2024-12-31 12:09:48 +0530

    output video fiile added

--- main.py
+++ main.py
@@ -1,57 +1,42 @@
 import cv2
+import os
 from mediapipe.python.solutions import drawing_utils
 import mediapipe.python as mp
+import argparse
 from mediapipe.python.solutions import pose 
 from utils.drawing_utils import draw_landmarks
 from utils.body_parts import LeftSideBodyParts as L, RightSideBodyParts as R, FullBody as F
+
+# Initialize Parser
+parser = argparse.ArgumentParser()
+
+# Add Arguments
+parser.add_argument("-p", "--Path", help="Path to your video file")
+parser.add_argument("-o", "--Output", default="output", help="Path to output dir")
+
+# Read arguments from command line
+args = parser.parse_args()
 
 my_pose = pose.Pose(model_complexity=1, min_detection_confidence=0.9, min_tracking_confidence=0.9)
 
 # Initialize MediaPipe Drawing.
 mp_drawing = drawing_utils
-
-
-
-
-frozenset([                             (5, 6),(4, 5),          (1, 2), (2, 3),
-                                    (6, 8),                                     (3, 7),
-                                                    (0, 4),(0, 1),
-           
-
-
-                                                        (9, 10),
-                                
-
-
-
-                    (16, 22),                                                                   (15, 21),
-           (18, 20),(16, 20),    (14, 16),  (12, 14),   (11, 12),   (11, 13),    (13, 15),      (15, 19), (17, 19),
-                (16, 18),                                                                       (15, 17),
-                
-                
-
-
-
-                
-                                            (12, 24),   (23, 24),   (11, 23),
-                                            
-                                            
-                                            (24, 26),               (23, 25),
-
-
-                                            (26, 28),               (25, 27),
-
-
-                                            (28, 30),               (27, 31),
-
-                                    (28, 32), (30, 32),            (27, 29), (29, 31),
-                                            ])    
+  
 UpperBodyConnections = [R.right_bicep, R.right_forearm, R.right_torso, L.left_bicep, L.left_forearm, L.left_torso]
 ArcConnections = [list(R.right_forearm)+list(R.right_bicep), list(L.left_forearm)+list(L.left_bicep)]
 print(ArcConnections)
 
-# Open the video file or webcam.
-cap = cv2.VideoCapture('data\\back\SeatedCableRow.mp4')  # Replace 'input_video.mp4' with 0 to use the webcam.
+# Open the video file or webcam. 'data\\back\SeatedCableRow.mp4'
+cap = cv2.VideoCapture(args.Path)  # Replace 'input_video.mp4' with 0 to use the webcam.
+
+frame_width = int(cap.get(3)) 
+frame_height = int(cap.get(4)) 
+   
+size = (frame_width, frame_height) 
+
+result = cv2.VideoWriter(f'{args.Output}/{os.path.basename(args.Path)}.avi',  
+                         cv2.VideoWriter_fourcc(*'MJPG'), 
+                         30, size) 
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -73,13 +58,14 @@ while cap.isOpened():
     # Display the frame.
     cv2.imshow('Pose Detection', frame)
     # cv2.waitKey(0)
+    result.write(frame)
     # Press 'q' to exit the loop.
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 # Release the video capture object.
 cap.release()
-
+result.release() 
 # img = cv2.imread("girl.jpg")
 
 
